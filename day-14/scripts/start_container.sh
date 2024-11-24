c0c7e4b29d01c41f8d1639f6bf858315b915dfec
Vstar007 <98928150+Vstar007@users.noreply.github.com>
2024-11-23 18:53:05 -0700

    add the files for code deploy

---
+++ day-14/scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull vstar007/simple-python-flask-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 vstar007/simple-python-flask-app
